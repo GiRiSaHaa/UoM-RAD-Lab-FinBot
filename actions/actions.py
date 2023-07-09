7991e7727d970368f207fe5997e46eab4a3cc7e1
GiRi SaHaa <giri91sa@gmailcom>
2023-07-09 22:03:56 +0530

    Updating nlu and domain

--- actions/actions.py
+++ actions/actions.py
@@ -13,15 +13,15 @@
 # from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
+# class ActionDefaultFallback(Action):
 #
 #     def name(self) -> Text:
-#         return "action_hello_world"
+#         return "action_default_fallback"
 #
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         from rasa_sdk.events import UserUtteranceReverted
+#         dispatcher.utter_message(response='utter_other')
 #
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+#         return [UserUtteranceReverted()]
